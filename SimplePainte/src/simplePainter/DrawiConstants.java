4a6ec7c99c02684957794de9f03bac80de144714
40 <40@40-PC>
2018-11-29 14:01:08 +0900

    ADD contents to CONSTANTS

--- SimplePainte/src/simplePainter/DrawiConstants.java
+++ SimplePainte/src/simplePainter/DrawiConstants.java
@@ -4,6 +4,14 @@ import java.awt.Color;
 
 public class DrawiConstants {
 	
+	final public static int DOT =0;
+	final public static int LINE =1;
+	final public static int RECT =2;
+	final public static int OVAL =3;
+	
+	final public static int NONE =6;
+	
+	
 	final public static String[] MENU= {"DOT","LINE","RECT","OVAL"," "," "};
 	final public static Color[] MENU_EXIT= {Color.black, Color.white};
 	final public static Color[] MENU_ENTER= {Color.red, Color.white};
